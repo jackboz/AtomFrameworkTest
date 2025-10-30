7a9193325294116c5957402d7e2989afb4f54d3d
jackboz <jbotvinovski@gmail.com>
2025-10-30 14:38:43 -0400

    add update label method

--- Assets/UILogger.cs
+++ Assets/UILogger.cs
@@ -25,4 +25,16 @@ public class UILogger : MonoBehaviour
         _label1.text = _intValueList.List[0].ToString();
         _label2.text = _intValueList.List[1].ToString();
     }
+
+    public void UpdateLabels()
+    {
+        if (_intValueList.List.Count < 2)
+        {
+            Debug.LogError("IntValueList must have at least 2 values");
+            return;
+        }
+
+        _label1.text = _intValueList.List[0].ToString();
+        _label2.text = _intValueList.List[1].ToString();
+    }
 }
