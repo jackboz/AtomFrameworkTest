6795d789309c2a2293cdafc4a8d68d47385e15df
jackboz <jbotvinovski@gmail.com>
2025-10-30 13:15:42 -0400

    add changelistvalues script

---
+++ Assets/ChangeListValues.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityAtoms.BaseAtoms;
+
+public class ChangeListValues : MonoBehaviour
+{
+    [SerializeField] private IntValueList _intValueList;
+
+    void Start()
+    {
+        if (_intValueList == null)
+        {
+            Debug.LogError("IntValueList is not assigned");
+            return;
+        }
+
+        for (int i = 0; i < _intValueList.List.Count; i++)
+        {
+            Debug.Log("IntValueList[" + i + "] = " + _intValueList.List[i]);
+        }
+
+        _intValueList.List[0] = 1;
+    }
+}
