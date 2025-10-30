b150c97ebcbe3d5877430c077de54d2892898aad
jackboz <jbotvinovski@gmail.com>
2025-10-30 13:15:42 -0400

    add UI logger

---
+++ Assets/UILogger.cs
@@ -0,0 +1,28 @@
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILogger : MonoBehaviour
+{
+    [SerializeField] private IntValueList _intValueList;
+    [SerializeField] private Text _label1;
+    [SerializeField] private Text _label2;
+
+    void Start()
+    {
+        if (_intValueList == null)
+        {
+            Debug.LogError("IntValueList is not assigned");
+            return;
+        }
+
+        if (_intValueList.List.Count < 2)
+        {
+            Debug.LogError("IntValueList must have at least 2 values");
+            return;
+        }
+
+        _label1.text = _intValueList.List[0].ToString();
+        _label2.text = _intValueList.List[1].ToString();
+    }
+}
